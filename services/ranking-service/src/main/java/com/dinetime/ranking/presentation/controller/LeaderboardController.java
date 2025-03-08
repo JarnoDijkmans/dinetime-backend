ab897e949c5d8b923e818b489d2a4c234d9c5612
Jarno Dijkmans <jarnodijkmans@gmail.com>
2025-03-08 11:30:56 +0100

    Changed logic, expirementing with github

--- services/ranking-service/src/main/java/com/dinetime/ranking/presentation/controller/LeaderboardController.java
+++ services/ranking-service/src/main/java/com/dinetime/ranking/presentation/controller/LeaderboardController.java
@@ -4,12 +4,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.dinetime.ranking.presentation.request.VotesRequestModel;
+import com.dinetime.ranking.presentation.request.LeaderboardsRequestModel;
 
 
 @RestController
 @RequestMapping("/votes")
-public class RankingController {
+public class LeaderboardController {
     // private final IRankingService rankingService;
 
     // public RankingController(IRankingService rankingService) {
@@ -17,8 +17,8 @@ public class RankingController {
     // }
 
     @PostMapping("/batch")
-    public ResponseEntity<VotesRequestModel> batchSaveVotes(@RequestBody VotesRequestModel votes) {
-        return ResponseEntity.ok(votes);
+    public ResponseEntity<LeaderboardsRequestModel> batchSaveVotes(@RequestBody LeaderboardsRequestModel leaderboards) {
+        return ResponseEntity.ok(leaderboards);
         // rankingService.batchSaveVotes(votes);
         // return ResponseEntity.ok("✅ Votes stored successfully.");
     }
